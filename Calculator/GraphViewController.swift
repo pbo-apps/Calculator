df666b8f88d2ce5d2a69b8a5811a20e2f383fa2e
Pete Bounford <petebounford@gmail.com>
2017-02-11 14:10:34 +0100

    Add GraphViewController as detail view in split view

---
+++ Calculator/GraphViewController.swift
@@ -0,0 +1,23 @@
+//
+//  GraphViewController.swift
+//  Calculator
+//
+//  Created by Pete Bounford on 11/02/2017.
+//  Copyright © 2017 PBO Apps. All rights reserved.
+//
+
+import UIKit
+
+class GraphViewController: UIViewController {
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
